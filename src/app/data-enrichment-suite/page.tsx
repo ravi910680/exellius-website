ae645433f1023c524e639c4d6974a475befc1b4e
ravi910680 <ravigorasiya65@gmail.com>
2025-06-26 10:27:17 +0530

    Initial commit: Figma-based animation with Variant transitions 4

--- src/app/data-enrichment-suite/page.tsx
+++ src/app/data-enrichment-suite/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import Hero from "./Hero"
-import DataEnrichmentFeatures from "./DataEnrichmentFeatures"
 import DataEnrichmentFeature1 from "./DataEnrichmentFeature1"
 import DataEnrichmentFeature2 from "./DataEnrichmentFeature2"
 import DataEnrichmentFeature3 from "./DataEnrichmentFeature3"
